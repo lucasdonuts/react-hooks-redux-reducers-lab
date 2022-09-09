6d7866fd54f09dd471b143f24e438bef4c99f366
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-09 08:13:51 -0500

    commit tree 71d00c007433cfdfd2889ce4db26b9911cd1879b

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,10 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch(action.type){
+    case 'friends/add':
+      return { friends: [...state.friends, action.payload ] };
+    case 'friends/remove':
+      return { friends: state.friends.filter( f => f.id !== action.payload ) };
+    default:
+      return state;
+  }
 }
